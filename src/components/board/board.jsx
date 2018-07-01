6eb825c0c0d0f38fe4406fe8c7334197321dc143
sergeialimov <sv.alimov@yandex.ru>
2018-07-01 11:58:34 +0300

    add handleClick

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -4,9 +4,32 @@ import Square from '../square/square.jsx';
 import './board.css';
 
 export default class Board extends React.Component {
-  renderSquare (num) {
+  constructor () {
+    super();
+    this.state = {
+      arr: Array(9)
+        .fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick (i) {
+    const { arr, xIsNext } = this.state;
+    const arrCopy = arr.slice();
+    const value = xIsNext ? 'X' : 'O';
+    arrCopy[i] = value;
+    this.setState({
+      arr: arrCopy,
+      xIsNext: !xIsNext,
+    });
+  }
+
+  renderSquare (i) {
+    const { arr } = this.state;
     return (
-      <Square value={num} />
+      <Square
+        value={arr[i]}
+      />
     );
   }
 
@@ -14,35 +37,35 @@ export default class Board extends React.Component {
     return (
       <div className="board">
         <div className="row">
-          <div className="square">
+          <div className="square" onClick={() => this.handleClick(0)} onKeyDown={() => this.handleClick(0)}>
             {this.renderSquare(0)}
           </div>
-          <div className="square">
+          <div className="square" onClick={() => this.handleClick(1)} onKeyDown={() => this.handleClick(1)}>
             {this.renderSquare(1)}
           </div>
-          <div className="square">
+          <div className="square" onClick={() => this.handleClick(2)} onKeyDown={() => this.handleClick(2)}>
             {this.renderSquare(2)}
           </div>
         </div>
         <div className="row">
-          <div className="square">
+          <div className="square" onClick={() => this.handleClick(3)} onKeyDown={() => this.handleClick(3)}>
             {this.renderSquare(3)}
           </div>
-          <div className="square">
+          <div className="square" onClick={() => this.handleClick(4)} onKeyDown={() => this.handleClick(4)}>
             {this.renderSquare(4)}
           </div>
-          <div className="square">
+          <div className="square" onClick={() => this.handleClick(5)} onKeyDown={() => this.handleClick(5)}>
             {this.renderSquare(5)}
           </div>
         </div>
         <div className="row">
-          <div className="square">
+          <div className="square" onClick={() => this.handleClick(6)} onKeyDown={() => this.handleClick(6)}>
             {this.renderSquare(6)}
           </div>
-          <div className="square">
+          <div className="square" onClick={() => this.handleClick(7)} onKeyDown={() => this.handleClick(7)}>
             {this.renderSquare(7)}
           </div>
-          <div className="square">
+          <div className="square" onClick={() => this.handleClick(8)} onKeyDown={() => this.handleClick(8)}>
             {this.renderSquare(8)}
           </div>
         </div>
